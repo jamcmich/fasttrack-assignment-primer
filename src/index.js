b2889556773ed7fc0802fd78ce566128320ecd32
Jacob McMichael <jacobmcmichael@gmail.com>
2022-01-03 11:25:59 -0800

    Final submission. 3/3 tests passed!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 export function calculateTotal (items, tax) {
-  // TODO
+  let itemsArray = [...items];
+  let result = [];
+  
+  for (let item in itemsArray) {
+    // console.log(itemsArray[item]);
+
+    if (itemsArray[item].taxable === true) {
+      result.push((Math.abs(tax) * itemsArray[item].price) + itemsArray[item].price);
+    } else {
+      result.push(itemsArray[item].price);
+    }
+  }
+  // console.log(result);
+  return result.reduce((result, item) => result + item, 0);
 }
